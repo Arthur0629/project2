4e730e714c88ef3285f01965dfd8da5bddf702d8
Jui-Chieh Hsiao <n9326774@qut.edu.au>
2019-09-23 22:02:58 +1000

    update

--- src/app/recommendation/recommendation.page.ts
+++ src/app/recommendation/recommendation.page.ts
@@ -15,13 +15,7 @@ export class RecommendationPage implements OnInit {
   arrayOfItems:any [] = [];
   myProflie:any;
   matchingResult:any;
-<<<<<<< HEAD
-  idealMate : any;
-  idealGender : any;
-  idealHabit : any;
-=======
   
->>>>>>> dev-hanwen
 
   constructor(private navCtrl: NavController,
     public router: Router,
@@ -30,62 +24,6 @@ export class RecommendationPage implements OnInit {
 
   ngOnInit(){
     
-<<<<<<< HEAD
-    if( localStorage.getItem("idealMate" ) ){
-      this.idealMate = localStorage.getItem( "idealMate" );
-      this.idealMate = localStorage.getItem("idealAge");
-      this.idealGender = localStorage.getItem("idealGender");
-      this.idealHabit = localStorage.getItem("idealHabit");
-
-      
-    }
-    var xiaohong:any = new Object();
-    xiaohong.name = "xiaohong";
-    xiaohong.dob = "01/01/1981";
-    xiaohong.gender = "Female";
-    xiaohong.swmming = true;
-    xiaohong.baskball = true;
-
-    
-
-    var xiaoming:any = new Object();
-    xiaoming.name = "xiaoming";
-    xiaoming.dob = "01/01/1990";
-    xiaoming.gender = "Female";
-    xiaoming.swmming = false;
-    xiaoming.baskball = true;
-
-    var xiaobai:any = new Object();
-    xiaobai.name = "xiaohong";
-    xiaobai.dob = "01/08/1981";
-    xiaobai.gender = "Male";
-    xiaobai.swmming = true;
-    xiaobai.baskball = false;
-    
-    var xiaowang:any = new Object();
-    xiaowang.name = "xiaohong";
-    xiaowang.dob = "01/08/1981";
-    xiaowang.gender = "Male";
-    xiaowang.swmming = true;
-    xiaowang.baskball = false;
-
-    var zhangsan: any = new Object();
-    zhangsan.name = " zhangsan ";
-    zhangsan.dob = " 01/01/1982";
-    zhangsan.gender = "male";
-    zhangsan.swmming = true;
-    zhangsan.baskball = true;
-
-
-    this.arrayOfItems.push( xiaohong);
-    this.arrayOfItems.push( xiaoming);
-    this.arrayOfItems.push( xiaobai);
-    this.arrayOfItems.push( xiaowang);
-
-    this.myProflie = zhangsan;
-
-    this.matchingCalculator();
-=======
     this.databaseService.getUserCollection().subscribe(items =>{
       this.arrayOfItems = items;
       this.matchingCalculator();
@@ -93,7 +31,6 @@ export class RecommendationPage implements OnInit {
       var b = 2;
       
     });
->>>>>>> dev-hanwen
 
   }
 
