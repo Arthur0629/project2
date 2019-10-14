12d9600151aac364e2ea871deffb783016a53d2b
Jui-Chieh Hsiao <n9326774@qut.edu.au>
2019-10-14 20:33:56 +1000

    optmise email verification

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NavController, ModalController, LoadingController } from '@ionic/angular';
 import { AuthenticateService } from '../service/authentication.service';
 import { Router, ActivatedRoute } from '@angular/router';
-
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-home',
@@ -10,13 +10,21 @@ import { Router, ActivatedRoute } from '@angular/router';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  hasVerifiedEmail = true;
+  sentTimestamp;
 
   constructor(
     private navCtrl: NavController,
     public router: Router,
     public loadingCtrl: LoadingController,
     private authService: AuthenticateService,
-  ) {}
+    public afAuth: AngularFireAuth
+  ) {
+    this.afAuth.authState.subscribe(user => {
+      if (user)
+        this.hasVerifiedEmail = this.afAuth.auth.currentUser.emailVerified;
+    });
+  }
 
   gotoProfile(){
     this.router.navigateByUrl("profile")
@@ -28,6 +36,13 @@ export class HomePage {
 
   recommendationList(){
     this.router.navigateByUrl("recommendation")
+  }
+
+  sendEmail(){
+    this.authService.snedEmail();
+  }
+  reload() {
+    window.location.reload();
   }
 
   logout(){
